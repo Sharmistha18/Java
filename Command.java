a57927a50154672c01fd8e92da5f82b96f4763c8
Sharmistha Modak <43649366+Sharmistha18@users.noreply.github.com>
2020-10-02 08:11:50 +0530

    Add files via upload

---
+++ Command.java
@@ -0,0 +1,12 @@
+public class Command
+{
+    public static void main(String args[])
+    {
+        int n1,n2,s;
+        n1=Integer.parseInt("4");
+        n2=Integer.parseInt("10");
+        
+        s=n1+n2;
+        System.out.println("sum="+s);
+    }
+}
